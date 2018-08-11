7e51ddcc25372d584866dcfb2ebc9c19e757254d
DEV <DEV@LILLY>
2018-08-12 00:22:26 +0530

    added step2 in AppConfig

--- springboot-testing/src/main/java/com/arjun/boot/config/AppConfig.java
+++ springboot-testing/src/main/java/com/arjun/boot/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.arjun.boot.config;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,5 +15,11 @@ public class AppConfig {
 	@Bean
 	public Job job() {
 		return (Job) jobBuilderFactory.get("batchJOb");
+	}
+
+	@Bean
+	public Step step2() {
+		return null;
+
 	}
 }
