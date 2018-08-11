9babbd18f0808fc8bd6b78f51bdc893f124ebcf3
DEV <DEV@LILLY>
2018-08-11 23:48:08 +0530

    Config and added dependency in pom of batch

---
+++ springboot-testing/src/main/java/com/arjun/boot/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.arjun.boot.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Bean
+	public Job job() {
+		return (Job) jobBuilderFactory.get("batchJOb");
+	}
+}
