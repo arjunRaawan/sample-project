121aaa787b35047d23f6bd33136918ee230e5448
DEV <DEV@LILLY>
2018-08-12 00:58:56 +0530

    Added Setters and Getters

--- springboot-testing/src/main/java/com/arjun/boot/model/User.java
+++ springboot-testing/src/main/java/com/arjun/boot/model/User.java
@@ -9,4 +9,53 @@ public class User {
 	private String password;
 	private String last;
 	private Date dob;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
 }
